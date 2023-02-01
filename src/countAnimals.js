330bda0fb691d80be3eb89c0f444052607558817
Fabricio <fabricio12nastacio@gmail.com>
2023-02-01 19:22:42 -0300

    requisito 5 concluido - Procura animais pela especie/sexo ou todos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (paranobj) => {
+  if (paranobj === undefined) {
+    const obj = data.species.map((specie) => ({
+      [specie.name]: specie.residents.length,
+    })).sort();
+    return Object.assign(...obj);
+  }
+
+  const { species, sex } = paranobj;
+  const animais = data.species.find((specie) => specie.name === species);
+
+  if (sex === undefined) {
+    return animais.residents.length;
+  }
+  return animais.residents.filter((resident) => resident.sex === sex).length;
 };
 
 module.exports = countAnimals;

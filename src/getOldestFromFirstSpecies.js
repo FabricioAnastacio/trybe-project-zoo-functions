259e6b49041c611af25a728873372832953b9fe3
Fabricio <fabricio12nastacio@gmail.com>
2023-02-03 09:24:03 -0300

    Requisito 10 concluido -captura o animal mais velho que determinado colaborador cuida-

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const people = data.employees.find((employee) => employee.id === id);
+  const animal = data.species.find((specie) => specie.id === people.responsibleFor[0]);
+  const result = animal.residents.reduce((acc, crr) => {
+    if (acc.age > crr.age) {
+      return acc;
+    }
+    return crr;
+  });
+
+  return Object.values(result);
 };
 
 module.exports = getOldestFromFirstSpecies;
